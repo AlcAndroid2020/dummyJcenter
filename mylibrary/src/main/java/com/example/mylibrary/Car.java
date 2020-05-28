a4bfbd30499a7d9b56d7b7fd85806aae99ac8548
cstanALC <cstan.alc@gmail.com>
2020-05-28 16:27:43 +0800

    library for jcenter

---
+++ mylibrary/src/main/java/com/example/mylibrary/Car.java
@@ -0,0 +1,32 @@
+package com.example.mylibrary;
+
+
+public class Car {
+    private int Year;
+    private String name;
+    private String Type;
+
+    public int getYear() {
+        return Year;
+    }
+
+    public void setYear(int year) {
+        Year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+}
